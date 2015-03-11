aeb00020414ef5a8e2178c0734b3baab845437be
Andrew Titus <atitus@mit.edu>
2015-03-11 19:19:43 -0400

    Added first test of sidebar

---
+++ Test App/Test App/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  Test App
+//
+//  Created by Andrew Titus on 3/11/15.
+//  Copyright (c) 2015 GreekEasy. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //menuButton.addTarget(self.revealViewController(), action: "revealToggle:", forControlEvents: UIControlEvents.TouchUpInside)
+        menuButton.target = self.revealViewController()
+        menuButton.action = "revealToggle:"
+
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Test App/Test App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test App
+//
+//  Created by Andrew Titus on 3/11/15.
+//  Copyright (c) 2015 GreekEasy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
