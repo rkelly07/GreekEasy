fde80cea7f401417fcb4d54f84486333a442dc06
Andrew Titus <atitus@mit.edu>
2015-03-25 23:04:44 -0400

    Set up Location Logger with Parse and better GPS

--- Location Logger/Location Logger/ViewController.swift
+++ Location Logger/Location Logger/ViewController.swift
@@ -13,7 +13,6 @@ import MapKit
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var logButton: UIButton!
     @IBOutlet weak var modeSelector: UISegmentedControl!
     @IBOutlet weak var infoLabel: UILabel!
     
@@ -22,29 +21,57 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set default mode to WiFi: on, GPS: on
+        modeSelector.selectedSegmentIndex = 2
+        
         // Set up location manager
         manager = CLLocationManager()
         manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyKilometer
+        manager.distanceFilter = 10 // Mark location changes only after >10 m changes
+        manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         
         // Set up map view
         mapView.delegate = self
         mapView.mapType = MKMapType.Hybrid
         
-        infoLabel.text = "Latitude: 0\nLongitude: 0\nLatitude Accuracy: 0\nLongitude Accuracy: 0"
+        infoLabel.text = "Latitude: calculating...\nLongitude: calculating...\n" +
+            "Accuracy: calculating..."
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         let currentLocation = mapView.userLocation.location
         
         if currentLocation != nil {
+            // Display information on infoLabel
             let latitude = currentLocation.coordinate.latitude
             let longitude = currentLocation.coordinate.longitude
             let accuracy = currentLocation.horizontalAccuracy
             
             infoLabel.text = "Latitude: \(latitude)\nLongitude: \(longitude)\n" +
-                "Accuracy: \(accuracy)"
+            "Accuracy: \(accuracy)"
+            
+            // Log entry
+            self.logLocation(currentLocation, mode: modeSelector.selectedSegmentIndex)
+        }
+    }
+
+    func logLocation(loc: CLLocation, mode: Int) {
+        var info = PFObject(className: "Location")
+    
+        info["latitude"] = loc.coordinate.latitude
+        info["longitude"] = loc.coordinate.longitude
+        info["accuracy"] = loc.horizontalAccuracy
+        info["mode"] = mode
+    
+        info.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError!) -> Void in
+            if (success) {
+                // Object has been saved
+            } else {
+                // Problem; check error.description
+                NSLog(error.description)
+            }
         }
     }
     
@@ -54,19 +81,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
         
         let selectIndex = sender.selectedSegmentIndex
+
+        /*
         
-        // Set message and accuracy based on segment chosen
+        Set message and accuracy based on segment chosen
+
+        0: WiFi off, GPS off
+        1: WiFi on, GPS off
+        2: WiFi on, GPS on
+
+        */
+        
         switch selectIndex {
         case 0:
-            manager.desiredAccuracy = kCLLocationAccuracyKilometer
+            manager.stopUpdatingLocation()
+            manager.startMonitoringSignificantLocationChanges()
             alertController.title = "Wifi OFF, GPS OFF"
             alertController.message = "Please make sure that Wi-Fi is turned OFF"
         case 1:
-            manager.desiredAccuracy = kCLLocationAccuracyKilometer
+            manager.stopMonitoringSignificantLocationChanges()
+            manager.startUpdatingLocation()
             alertController.title = "Wifi ON, GPS OFF"
             alertController.message = "Please make sure that Wi-Fi is turned ON"
         case 2:
-            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.stopMonitoringSignificantLocationChanges()
+            manager.startUpdatingLocation()
             alertController.title = "Wifi ON, GPS ON"
             alertController.message = "Please make sure that Wi-Fi is turned ON"
         default:
@@ -82,10 +121,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             mapView.setUserTrackingMode(MKUserTrackingMode.Follow, animated: true)
             manager.startUpdatingLocation()
         }
-    }
-    
-    func logLocation(locationInfo: String) {
-        
     }
     
     override func didReceiveMemoryWarning() {
