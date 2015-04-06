db9fe2ac60122c8e02288d5a915cb7ddbea6a876
Ryan Kelly <rhkelly@mit.edu>
2015-04-06 13:29:32 -0400

    worked on reimburse

--- Test App/Test App/ReimburseCreateViewController.swift
+++ Test App/Test App/ReimburseCreateViewController.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class ReimburseCreateViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var amountField: UITextField!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var createButton: UIButton!
+    
+}
